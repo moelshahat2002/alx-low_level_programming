2dbb34533ac85412889f49f0cdb1b2c3b2988fb0
Mohammed Elshahat Ismail <122821360+moelshahat2002@users.noreply.github.com>
2023-02-18 01:18:58 +0200

    Create 0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%d is ", n);
+
+	if (n > 0)
+	{
+		printf("positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("zero\n");
+	}
+	else
+	{
+		printf("negative\n");
+	}
+	return (0);
+}
