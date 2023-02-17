b18a6fe04f50693506deb34f9abf887b870713f1
Mohammed Elshahat Ismail <122821360+moelshahat2002@users.noreply.github.com>
2023-02-18 00:54:19 +0200

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf: prints a string followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
