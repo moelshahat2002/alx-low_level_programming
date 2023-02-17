dba730e2f87d0e69d83d037fae008b9738807f84
Mohammed Elshahat Ismail <122821360+moelshahat2002@users.noreply.github.com>
2023-02-18 00:53:46 +0200

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * puts: prints "Programming is like building a multiligual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
